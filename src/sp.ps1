714ce91fc1d6c155c859076cbdb4c29acc6acad2
Aaron Veil <70171475+anddea@users.noreply.github.com>
2023-05-23 18:16:15 +0300

    Remove Upgrade button

--- src/sp.ps1
+++ src/sp.ps1
@@ -15,7 +15,7 @@ $reader = New-Object System.IO.StreamReader($entry_xpui.Open())
 $xpui_js = $reader.ReadToEnd()
 $reader.Close()
 
-$xpui_js_new = $xpui_js -replace 'adsEnabled:!0', 'adsEnabled:!1' -replace 'allSponsorships' , '' -replace 'i=e=>"free"', 'i=e=>"premium"' -replace 's=e=>"premium"', 's=e=>"free"' -replace 'withQueryParameters\(e\){return this.queryParameters=e,this}', 'withQueryParameters(e){return this.queryParameters=(e.types?{...e, types: e.types.split(",").filter(_ => !["episode","show"].includes(_)).join(",")}:e),this}' -replace ',this[.]enableShows=[a-z]', ''
+$xpui_js_new = $xpui_js -replace 'adsEnabled:!0', 'adsEnabled:!1' -replace 'allSponsorships' , '' -replace '(.=.=>)"free"', '$1"premium"'
 
 $writer = New-Object System.IO.StreamWriter($entry_xpui.Open())
 $writer.BaseStream.SetLength(0)
